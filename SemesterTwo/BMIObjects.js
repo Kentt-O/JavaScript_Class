2e68f6aaa528855094d3b5afef42b5e4b68868d0
Kentt-O <kentc.okoro@gmail.com>
2024-07-03 14:07:50 +0100

    Updated

--- SemesterTwo/BMIObjects.js
+++ SemesterTwo/BMIObjects.js
@@ -11,20 +11,20 @@ const mark = {
 };
 
 const john = Object.create(mark);
-    fullName : 'John Smith';
-    mass : 92;
-    height : 1.95;
+john.fullName = 'John Smith';
+john.mass = 92;
+john.height = 1.95;
 
-    
+
 
 mark.calcBMI();
 john.calcBMI();
 console.log(mark.bmi, john.bmi);
 
 if (mark.bmi > john.bmi){
-    console.log(`${mark.fullName}'s BMI (${mark.bmi}) 
-    is higher than ${john.fullName}'s BMI (${john.bmi})`)
+    console.log(`${mark.fullName}'s BMI "${mark.bmi}"\nis higher than ${john.fullName}'s BMI "${john.bmi}"`)
 } else if (john.bmi > mark.bmi){
-    console.log(`${john.fullName}'s BMI (${john.bmi})
-    is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+    console.log(`${john.fullName}'s BMI "${john.bmi}"\nis higher than ${mark.fullName}'s BMI "${mark.bmi}"`)
+}else {
+    console.log(`Both ${mark.fullName} and ${john.fullName} have the same BMI`);
 }
