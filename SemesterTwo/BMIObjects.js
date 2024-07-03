cb72159fdba4e860e74fcf8a6d47e323e4328cb7
Kentt-O <kentc.okoro@gmail.com>
2024-07-03 13:25:05 +0100

    updatin

--- SemesterTwo/BMIObjects.js
+++ SemesterTwo/BMIObjects.js
@@ -1,14 +1,27 @@
 const mark = {
-    firstName : 'Mark',
-    lastName : 'Miller',
-    mass : ,
-    height :
+    fullName : 'Mark Miller',
+    mass : 78,
+    height : 1.69,
+
+    calcBMI : function (){
+        this.bmi = this.mass / (this.height ** 2)
+        return this.bmi
+    },
 
 };
 
 const john = {
-    firstName : 'John',
-    lastName :  'Smith',
-    mass : ,
-    height :
-}
+    fullName : 'John Smith',
+    mass : 92,
+    height : 1.95,
+
+    calcBMI : function (){
+        this.bmi = this.mass / (this.height ** 2)
+        return this.bmi
+    },
+
+};
+
+mark.calcBMI();
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
