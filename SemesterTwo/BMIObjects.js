8f736c0c62ace5d9e5e122acd1d1ad3d83f0edca
Kentt-O <kentc.okoro@gmail.com>
2024-07-03 12:49:49 +0100

    Update

---
+++ SemesterTwo/BMIObjects.js
@@ -0,0 +1,14 @@
+const mark = {
+    firstName : 'Mark',
+    lastName : 'Miller',
+    mass : ,
+    height :
+
+};
+
+const john = {
+    firstName : 'John',
+    lastName :  'Smith',
+    mass : ,
+    height :
+}
---
+++ SemesterTwo/BMIobjects.test.js
@@ -0,0 +1,1 @@
+const {returnTestScoresAboveSeventy} = require("./func.js");
